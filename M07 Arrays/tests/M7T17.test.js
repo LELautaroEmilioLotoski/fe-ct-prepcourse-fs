e4d78a3be451eff9a46a85db76e7d154429890b6
unknown <lautilotoski@gmail.com>
2024-01-24 17:53:06 -0300

    a Terminar

--- M07 Arrays/tests/M7T17.test.js
+++ M07 Arrays/tests/M7T17.test.js
@@ -1,9 +1,5 @@
-const breakStatement = require('../ejercicios/18');
+const tablaDelSeis = require('../ejercicios/17');
 
-test('Debe retornar un arreglo con la tabla de multiplicar del 6', function () {
-  expect(breakStatement(60)).toEqual([0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60]);
-});
-
-test('La ejecución se debe interrumpir en el caso pedido', function () {
-  expect(breakStatement(61)).toEqual('Se interrumpió la ejecución');
+test('Debe retornar la tabla de multiplicar del 6 en un arreglo', function () {
+  expect(tablaDelSeis()).toEqual([0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60]);
 });
