b8e4f15db6c9524cd8f8c807c997ca4efcceeab7
unknown <lautilotoski@gmail.com>
2024-01-26 17:31:30 -0300

    terminado

--- M07 Arrays/ejercicios/30.js
+++ M07 Arrays/ejercicios/30.js
@@ -2,5 +2,12 @@ function encontrarElementoRepetido(numeros) {
   // La función recibe un argumento "numeros" que es un array de números.
   // Retorna el primer elemento repetido que se encuentre en el array.
   // Tu código:
-}s
+  numeros.sort();
+  for(let i = 0; i < numeros.length; i++){
+    if(numeros[i] === numeros[i + 1]){
+      return numeros[i];
+    }
+  }
+}
+encontrarElementoRepetido([1, 2, 3, 3, 4, 5, 6]);
 module.exports = encontrarElementoRepetido;
