18bd44537b8d5f42945e521e016b9a67486a5f4c
unknown <lautilotoski@gmail.com>
2024-02-01 20:17:29 -0300

    aTerminar

--- M08 Objects + Callbacks/objects/ejercicios/01.js
+++ M08 Objects + Callbacks/objects/ejercicios/01.js
@@ -2,6 +2,11 @@ const obtenerValorPropiedad = (objeto, propiedad) => {
   // Recibe un objeto y el nombre de una propiedad.
   // Devuelve el valor de esta propiedad.
   // Tu código:
-};
+  if(objeto.hasOwnProperty(propiedad)){
+    return objeto[propiedad];
+  }else{
+    return undefined;
+  }
+}
 
 module.exports = obtenerValorPropiedad;
--- M08 Objects + Callbacks/objects/ejercicios/02.js
+++ M08 Objects + Callbacks/objects/ejercicios/02.js
@@ -2,6 +2,24 @@ function actualizarValorPropiedad(objeto, propiedad, valor) {
   // Actualiza el valor de la propiedad del objeto recibidos en la función.
   // Retorna el objeto actualizado.
   // Tu código:
+  for(let prop in objeto){
+    prop = propiedad;
+    if(objeto.hasOwnProperty(prop)){
+      let nuevoObjeto = {...objeto};
+      nuevoObjeto[prop] = valor;
+      return nuevoObjeto;
+    }else if(!objeto.hasOwnProperty(prop)){
+      let newObjeto = {...objeto};
+      newObjeto[prop] = valor;
+      return newObjeto;
+    }
+    return objeto;
+  }
 }
-
+let casa = {
+  calle: 'falsa',
+  direccion: 123
+}
+actualizarValorPropiedad(casa, 'casa.calle', 'verdadera');
+console.log(casa);
 module.exports = actualizarValorPropiedad;
